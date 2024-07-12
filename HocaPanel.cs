da80f1065e386d27a7c636e03ea789a2191ef5c6
Kaan Kutay KÖSE <110459413+kaankutaykose@users.noreply.github.com>
2024-07-12 14:07:14 +0300

    Update HocaPanel.cs

--- HocaPanel.cs
+++ HocaPanel.cs
@@ -8,4 +8,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+namespace PostgreSQL
+{
+    public partial class HocaPanel : Form
+    {
+        private string hocaKimlik;
+        public HocaPanel(string hocaKimlik)
+        {
+            InitializeComponent();
+            this.hocaKimlik = hocaKimlik;
+        }
+        NpgsqlConnection connection = new NpgsqlConnection("server=localhost; port=5432;" +
+               "Database=obs; user Id=postgres; password=admin;");
+
+        private void ilgiAlanEkle_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            string insertSql = "UPDATE hoca SET ilgialan = @ilgialan WHERE hocaid = @hocaid";
+            using (NpgsqlCommand ilgiAlanEkle = new NpgsqlCommand(insertSql, connection))
+            {
+                ilgiAlanEkle.Parameters.AddWithValue("@ilgialan", txtilgiAlan.Text);
+                ilgiAlanEkle.Parameters.AddWithValue("@hocaid", int.Parse(hocaKimlik));
+                ilgiAlanEkle.ExecuteNonQuery();
+                MessageBox.Show("İlgi alanı eklendi");
+            }
+            connection.Close();
+        }
 
